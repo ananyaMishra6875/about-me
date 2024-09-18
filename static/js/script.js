67386fa9d40f8ef828f52fb517be6a934e1f6a7c
ananyaMishra6875 <ananya.diva@gmail.com>
2024-09-18 11:26:58 -0400

    24.09.18 11:26

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,17 @@
-function showMessage() {
-    let message = "Hello, Ananya!";
-    document.getElementById('message').innerHTML = message;
-}
+// function showMessage() {
+//     let message = "Hello, Ananya!";
+//     document.getElementById('message').innerHTML = message;
+// }
+
+// let page = "home";
+// const pages = ["home","about-me","project","skills"];
+// const aboutLink = document.getElementById('aboutBtn');
+
+// function pageStatus() {
+//     pages.forEach(p => {
+//         document.getElementById(p).style.display = (page === p) ? "block" : "none";
+//     });
+// }
+
+// page = "about-me";
+// pageStatus();
